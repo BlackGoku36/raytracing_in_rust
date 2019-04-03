6b2aa40ba409e867b996256892e90f63f2036efd
Urjasvi <urjasvisuthar@gmail.com>
2019-04-03 20:44:18 +0530

    Chapter 3

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,30 @@
 pub mod raytrace;
 
 use raytrace::vec::Vec3;
+use raytrace::ray::Ray;
+
+fn color(r:Ray)-> Vec3{
+    let unit_direction = Vec3::make_unit_vector(r.direction());
+    let t = 0.5 * (unit_direction.y() + 1.0);
+    (1.0-t) * Vec3::new(1.0, 1.0, 1.0) + t*Vec3::new(0.5, 0.7, 1.0)
+}
 
 fn main() {
-    let nx: u16 = 200;
-    let ny: u16 = 100;
-
+    let nx = 200;
+    let ny = 100;
     print!("P3\n{} {}\n255\n", nx, ny);
+
+    let lower_left_corner = Vec3::new(-2.0, -1.0, -1.0);
+    let horizontal = Vec3::new(4.0, 0.0, 0.0);
+    let vertical = Vec3::new(0.0, 2.0, 0.0);
+    let origin = Vec3::new(0.0, 0.0, 0.0);
 
     for j in (0..ny).rev() {
         for i in 0..nx {
-            let col = Vec3::new(i as f32 / nx as f32, j as f32 / ny as f32, 0.2);
+            let u = i as f32 / nx as f32;
+            let v = j as f32 / ny as f32;
+            let r = Ray::new(origin, lower_left_corner + u*horizontal + v*vertical);
+            let col = color(r);
             let ir = (255.99 * col[0]) as i32;
             let ig = (255.99 * col[1]) as i32;
             let ib = (255.99 * col[2]) as i32;
---
+++ src/raytrace/ray.rs
@@ -0,0 +1,25 @@
+use super::vec::Vec3;
+
+#[derive(Copy, Clone, Debug)]
+pub struct Ray{
+    origin: Vec3,
+    direction: Vec3,
+}
+
+impl Ray{
+    pub fn new(origin: Vec3, direction:Vec3)-> Self{
+        Ray{origin, direction}
+    }
+
+    pub fn origin(&self)->Vec3{
+        self.origin
+    }
+
+    pub fn direction(&self)->Vec3{
+        self.direction
+    }
+
+    pub fn point_at_parameter(&self, t: f32)->Vec3{
+        self.origin + t * self.direction
+    }
+}
