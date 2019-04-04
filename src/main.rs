6bae239920e32c1ace403d334b78de7c2171c9c1
Urjasvi <urjasvisuthar@gmail.com>
2019-04-04 11:26:26 +0530

    Chapter 5a(Surface Normal)

--- src/main.rs
+++ src/main.rs
@@ -3,19 +3,24 @@ pub mod raytrace;
 use raytrace::vec::Vec3;
 use raytrace::ray::Ray;
 
-fn hit_sphere(center: Vec3, radius: f32, r: Ray)-> bool{
+fn hit_sphere(center: Vec3, radius: f32, r: Ray)-> f32{
     let oc:Vec3 = r.origin() - center;
     let a = Vec3::dot(&r.direction(), &r.direction());
     let b = 2.0*Vec3::dot(&oc, &r.direction());
     let c = Vec3::dot(&oc, &oc) - radius*radius;
     let discriminant = b*b - 4.0*a*c;
-    discriminant > 0.0
+    if discriminant < 0.0{
+        -1.0
+    }else{
+        (-b-f32::sqrt(discriminant))/(2.0*a)
+    }
 }
 
 fn color(r:Ray)-> Vec3{
-    if hit_sphere(Vec3::new(0.0, 0.0, -1.0), 0.5, r){
-        let output = Vec3::new(1.0, 0.0, 0.0);
-        return output;
+    let t = hit_sphere(Vec3::new(0.0, 0.0, -1.0), 0.5, r);
+    if t > 0.0{
+        let n = Vec3::make_unit_vector(r.point_at_parameter(t) - Vec3::new(0.0, 0.0, -1.0));
+        return 0.5*Vec3::new(n.x()+1.0, n.y()+1.0, n.z()+1.0);
     }
     let unit_direction = Vec3::make_unit_vector(r.direction());
     let t = 0.5 * (unit_direction.y() + 1.0);
